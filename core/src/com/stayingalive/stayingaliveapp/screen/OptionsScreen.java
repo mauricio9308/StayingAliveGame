89df57ca9e4ebc2daa3340e4471ffd3a6f10d8e5
Kevin Cabrera <kevin.cabrera.argaez@gmail.com>
2014-12-06 17:59:41 -0600

    BackButtonImages

--- core/src/com/stayingalive/stayingaliveapp/screen/OptionsScreen.java
+++ core/src/com/stayingalive/stayingaliveapp/screen/OptionsScreen.java
@@ -3,10 +3,13 @@ package com.stayingalive.stayingaliveapp.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -42,6 +45,9 @@ public class OptionsScreen extends AbstractScreen {
         Table table = super.getTable();
         setBackground( "background-all.png");
 
+        table.left().top();
+
+
         final CheckBox musicCheckbox = new CheckBox("Music", getSkin());
         musicCheckbox.setChecked( mPreferencesManager.isMusicEnabled() );
         musicCheckbox.addListener(new ChangeListener() {
@@ -69,9 +75,19 @@ public class OptionsScreen extends AbstractScreen {
             }
         });
 
-        final TextButton back = new TextButton("Back", getSkin() );
-        back.setSize(48,48);
-        back.addListener( new ClickListener(){
+
+        Image backArrow = new Image( getGame().getAssetManager().get("uiskin/uiskin.atlas", TextureAtlas.class).findRegion("BackArrow") );
+
+        Button.ButtonStyle buttonsStyle = new Button.ButtonStyle();
+        buttonsStyle.up = backArrow.getDrawable();
+        buttonsStyle.checked = backArrow.getDrawable();
+        buttonsStyle.down = backArrow.getDrawable();
+
+        final Button backButton = new Button(buttonsStyle);
+
+//        final TextButton back = new TextButton("Back", getSkin() );
+        backButton.setSize(48,48);
+        backButton.addListener( new ClickListener(){
             @Override
             public void clicked( InputEvent event, float x, float y ){
                 getGame().setScreen( new MainScreen( getGame() ));
@@ -79,7 +95,7 @@ public class OptionsScreen extends AbstractScreen {
         });
 
         //first row
-        table.add( back ).left().padBottom(100).expandX();
+        table.add( backButton ).left().padBottom(100).expandX();
         table.row();
 
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator( Gdx.files.internal("fonts/Prisma.ttf"));
